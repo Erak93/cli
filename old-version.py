d384c05e4ce13398b00edbc57419f7550d204be6
Erak93 <giannicolaciudino@yahoo.it>
2022-11-11 15:43:10 +0100

    commit

--- old-version.py
+++ old-version.py
@@ -24,38 +24,30 @@ if operation=="1":
     print()
     print (*warehouse2, sep='\n')
 elif operation=="2":
-    subs=input("What are you looking for?")
-    res = [i for i in warehouse1 if subs in i]
-    res2=[i for i in warehouse2 if subs in i]
-    if res or res2:
-        print ("Here is your search match",*res , sep="\n")
-       
-        item_name=str.capitalize(input("Please pick an item: "))
-        print("In warehouse 1:")       
-        print("item count=",warehouse1.count(item_name))
-        print("In warehouse 2: ")
-        print("item count=",warehouse2.count(item_name))
-        if (warehouse1.count(item_name)+warehouse2.count(item_name))>=1:
-            print("Okay, you selected: ",item_name)
-            
-            order_decision=input("Do you want to order the item? YES or NO? ")
+    item_name=str.capitalize(input("Please input an item name: "))
+    print("In warehouse 1:")
+    print("item count=",warehouse1.count(item_name))
+    print("In warehouse 2: ")
+    print("item count=",warehouse2.count(item_name))
+    if (warehouse1.count(item_name)+warehouse2.count(item_name))>=1:
+        print("Okay, you selected: ",item_name)
         
-            if order_decision=="YES":
-                order_amount=int(input("How many items do you want to order? "))
-                if order_amount <=(warehouse1.count(item_name)+warehouse2.count(item_name)):
-                    print(order_amount,item_name,"ordered")
-                elif order_amount >(warehouse1.count(item_name)+warehouse2.count(item_name)):    
-                    supplemental_order=input("Not enough items in warehouse. Do you want to order the maximum availability instead? YES or NO: ")
-                    if supplemental_order=="YES":
-                                print(warehouse1.count(item_name)+warehouse2.count(item_name),item_name,"ordered.")
-                else:
-                    print("Program terminated")
-            elif order_decision=="NO":
-                breakpoint
+        order_decision=input("Do you want to order the item? YES or NO? ")
+    
+        if order_decision=="YES":
+            order_amount=int(input("How many items do you want to order? "))
+            if order_amount <=(warehouse1.count(item_name)+warehouse2.count(item_name)):
+                print(order_amount,item_name,"ordered")
+            elif order_amount >(warehouse1.count(item_name)+warehouse2.count(item_name)):    
+                supplemental_order=input("Not enough items in warehouse. Do you want to order the maximum availability instead? YES or NO: ")
+                if supplemental_order=="YES":
+                             print(warehouse1.count(item_name)+warehouse2.count(item_name),item_name,"ordered.")
             else:
-                print("Items not found.")
+                print("Program terminated")
+        elif order_decision=="NO":
+             breakpoint
     else:
-        print("Items not found")        
+        print("Items not found.")
         
 elif operation=="3":
     print("You exited the program")
---
+++ test.py
@@ -0,0 +1,10 @@
+list=["old laptop","phone", "laptop red"]
+subs=input("What do you want?")
+res = [i for i in list if subs in i]
+print (res)
+
+
+
+
+
+
